15f383f77c078576fe0bf2d0cc72b7573028af8b
Sharif Aly <sharif.aly_@outlook.com>
2023-09-05 20:47:25 +0200

    post request is working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,64 @@
 import "./App.css";
-// import { useEffect, useState } from "react";
+import Axios from "axios";
+import { useState } from "react";
 
 const App = () => {
+  const [Title, setTitle] = useState("");
+  const [Description, setDescription] = useState("");
+  const [Cover, setCover] = useState("");
+  const [Price, setPrice] = useState("");
+
+  const submitReview = () => {
+    Axios.post("http://localhost:4000/book/insert", {
+      Title: Title,
+      Description: Description,
+      Cover: Cover,
+      Price: Price,
+    }).then(() => {
+      alert("success");
+    });
+  };
+
   return (
-    <>
-      <h1>hello</h1>
-      <label htmlFor="">Book Name</label>
-      <input type="text" name="" id="" />
-      <label htmlFor="">Book Review</label>
-      <input type="text" name="" id="" />
-      <button>Submit</button>
-    </>
+    <div className="wrap">
+      <label htmlFor="">Book Title</label>
+      <input
+        type="text"
+        name=""
+        id=""
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <label htmlFor="">Book Description</label>
+      <input
+        type="text"
+        name=""
+        id=""
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
+      <label htmlFor="">Book Cover</label>
+      <input
+        type="text"
+        name=""
+        id=""
+        onChange={(e) => {
+          setCover(e.target.value);
+        }}
+      />
+      <label htmlFor="">Book Price</label>
+      <input
+        type="text"
+        name=""
+        id=""
+        onChange={(e) => {
+          setPrice(e.target.value);
+        }}
+      />
+      <button onClick={submitReview}>Submit</button>
+    </div>
   );
 };
 
